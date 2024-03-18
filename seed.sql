22f6667d516c1c63045e5e984209a02bcb7f8e6c
PhilipK-webdev <philipk179@gmail.com>
2024-03-18 11:53:33 +0200

    adding seed.sql file

---
+++ seed.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    uid INT AUTO_INCREMENT PRIMARY KEY,
+    uFullName VARCHAR(255) NOT NULL,
+    uPhoneNumber VARCHAR(255) NOT NULL,
+    uPicture VARCHAR(255),
+    uGender VARCHAR(255) NOT NULL,
+    uTitle VARCHAR(255) NOT NULL
+);
